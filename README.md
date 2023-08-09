be90635a445e29dc424a93e323730047fa16c09b
Uo <102210447+Uo1428@users.noreply.github.com>
2023-08-09 06:00:29 +0000

    v2

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
 <h1 align="center">
-   Another Journey 
+   AnotherJourney - Discord Bot
 </h1>
-<h4 align="center">Discord Bot that can create AI images like midJourney - Free</h4>
-<br>
+<h4 align="center">Made with love by Uo (dont take credit)</h4>
 
- To setupt bot watch [Tutorial](https://youtu.be/cjTMgRGKKE0)
---
-
+---------
+# How To Setup Bot?
+- Watch [**Youtbe Video**](https://youtu.be/T3aQ58JLVXM)
 # 💖 Support
-- ### [ΛLL IN ONΞ™ | Development </>](https://discord.gg/uoaio)
+- ### [ΛLL IN ONΞ™ | Development </>](https://dsc.gg/uoaio)
 # 💝 Credit
-If consider using this Template, make sure to credit me!
-### [Uo#0331](https://discord.com/users/922120042651451423) | [Discord](https://discord.gg/uoaio)
+Make sure to credit me!
+### [Uo#0331 or uoaio](https://discord.com/users/922120042651451423) | [Website](https://uoaio.vercel.app/) | [Discord](https://dsc.gg/uoaio)
 #### Profile 
 <img align="left" width="100%" style="margin: 0 10px 0 0;" alt=" " src="https://discord.c99.nl/widget/theme-2/922120042651451423.png">
 <img align="left" width="100%" style="margin: 0 10px 0 0;" alt=" " src="https://media.discordapp.net/attachments/877859563016159252/879664980218249216/colour_line2.gif">
--- index.js
+++ index.js
@@ -1,29 +1,98 @@
-const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
-require(`colors`)
-const client = new Client({
-  allowedMentions: {
-    parse: ["roles", "users", "everyone"],
-    repliedUser: false,
-  },
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildPresences,
-    GatewayIntentBits.GuildMessageReactions,
-    GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.MessageContent
-  ],
-  partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction]
-});
+/*
+==========| Dont Take Credit
+==========| Made By discord.gg/uoaio 
+*/
+
+import { Client, GatewayIntentBits, Partials, Collection, PermissionsBitField, EmbedBuilder, ButtonBuilder, ActionRowBuilder } from 'discord.js';
+import 'colors';
+import { Routes } from 'discord-api-types/v9';
+import { createMidjourney } from "replicate-fetch";
+import { REST } from '@discordjs/rest';
+import express from "express"
+const app = express()
+keepAlive();
+(async () => {
+  const client = new Client({
+    allowedMentions: {
+      parse: ["roles", "users", "everyone"],
+      repliedUser: false,
+    },
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildPresences,
+      GatewayIntentBits.GuildMessageReactions,
+      GatewayIntentBits.DirectMessages,
+      GatewayIntentBits.MessageContent
+    ],
+    partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction]
+  });
+
+  client.slashCommands = new Collection();
 
 
-[`variables`, `extraEvents`, `checker`, `server`, 'slashCommand', 'events', `antiCrash`].forEach((handler) => {
-  const file = require(`./src/handlers/${handler}`)
-  if (file.execute) file.execute(client);
-  else file(client);
-});
+  const TOKEN = process.env.TOKEN;
+  const CLIENT_ID = process.env.CLIENT_ID;
+  const rest = new REST({ version: '9' }).setToken(TOKEN);
+  let cmd = []
+  console.log("Reloading Commands.....")
+  const imagine = {
+    name: "imagine",
+    description: "Create AI images for free",
+    options: [{ name: "prompt", description: "Add promt to make image", type: 3, required: true }],
+    type: 1,
+  }
+  cmd.push(imagine)
+  client.slashCommands.set(imagine.name, imagine)
+  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: cmd }).then(() => console.log("Reloaded Commands".cyan.bold))
+  client.on('interactionCreate', async interaction => {
+    const slashCommand = client.slashCommands.get(interaction.commandName);
+    if (slashCommand.name !== "imagine") return;
+    await interaction.deferReply({ ephemeral: true })
+    await interaction.editReply({ content: "## Generating Image, Wait for a while\n\n> *join our discord if wont generate - https://discord.gg/uoaio *" })
+    const prompt = interaction.options.getString("prompt")
+    let x = await createMidjourney({ prompt });
+    if (!x || !x[0]) return interaction.editReply({ content: "Use Another Prompt!" })
+    const row = new ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder()
+          .setLabel("Download")
+          .setStyle(5)
+          .setURL(x[0])
+          .setEmoji("📥"),
+        new ButtonBuilder()
+          .setLabel("Support")
+          .setStyle(5)
+          .setURL("https://discord.gg/uoaio")
+      );
 
-client.login(client.config.TOKEN).catch((error) => { console.log((error.message).bold.red) });
+    const embed = new EmbedBuilder()
+      .setTitle("AnotherJourney:")
+      .setDescription(`**${prompt}**`)
+      .setImage(x[0])
+    await interaction.editReply({
+      embeds: [embed],
+      components: [row],
+      content: ""
+    })
+  })
+  
+client.on("ready", () => console.log("Bot Is Ready. Enjoy"))
+  
+  await client.login(process.env.TOKEN).catch((error) => {
+    console.log((error.message).bold.red);
+  });
+  
+})();
 
-
-module.exports = client;
+function keepAlive() {
+  app.use(express.static("public"));
+  app.get("/", (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.send(`<iframe style="margin: 0; padding: 0;" width="100%" height="100%" src="https://uoaio.vercel.app/" frameborder="0" allowfullscreen></iframe>`);
+    res.end()
+  });
+  app.listen(3000, () => {
+    console.log("Server is running on 3000".cyan)
+  })
+}
